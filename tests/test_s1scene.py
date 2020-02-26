cf6a06689965e5b4c71f064199bad880551164b9
Petr Ševčík <petr.sevcik@eox.at>
2020-02-26 16:24:13 +0100

    dev commit

--- tests/test_s1scene.py
+++ tests/test_s1scene.py
@@ -4,5 +4,5 @@ from ost import Sentinel1_Scene
 def test_s1scene_metadata(s1_id):
     s1 = Sentinel1_Scene(s1_id)
     s1.info()
-    control_id = 'S1A_IW_GRDH_1SDV_20191116T170638_20191116T170703_029939_036AAB_070F'
+    control_id = 'S1A_IW_GRDH_1SDV_20141003T040550_20141003T040619_002660_002F64_EC04'
     assert s1.scene_id == control_id
