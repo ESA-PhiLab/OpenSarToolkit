739c4e048e60146c3a06a5a2f019aad62d97af7c
Petr Ševčík <petr.sevcik@eox.at>
2020-02-27 09:06:58 +0100

    enable dl tests for scihub and asf

--- tests/test_s1_download.py
+++ tests/test_s1_download.py
@@ -40,9 +40,9 @@ def test_esa_scihub_connection(s1_grd_notnr_ost_product):
     assert uuid == control_uuid
 
 
-@pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
-                    reason="Skipping this test on Travis CI."
-                    )
+# @pytest.mark.skipif("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+#                     reason="Skipping this test on Travis CI."
+#                     )
 def test_esa_scihub_download(s1_grd_notnr_ost_product, mirror=1):
     herbert_uname = HERBERT_USER['uname']
     herbert_password = HERBERT_USER['pword']
@@ -65,9 +65,6 @@ def test_esa_scihub_download(s1_grd_notnr_ost_product, mirror=1):
         assert return_code is None
 
 
-@pytest.mark.skip(
-    reason="ASF download is tested in the test_project batch whatever!"
-)
 def test_asf_download(s1_grd_notnr_ost_product, mirror=2):
     herbert_uname = HERBERT_USER['uname']
     herbert_password = HERBERT_USER['asf_pword']
