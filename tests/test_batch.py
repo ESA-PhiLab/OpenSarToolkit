f3b3f00875182981cdc94102d22e058d278e6f95
Petr Sevcik <sevcik.cartography@gmail.com>
2020-04-03 15:41:18 +0200

    split tests

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -3,16 +3,26 @@ from ost.helpers.settings import config_check
 
 # Test burst batch for all slc ARD types
 # [OST-GTC, OST-RTC, OST-minimal]
-def test_burst_batch(slc_project_class):
-    for ard_type in config_check['type']['choices']:
-        slc_project_class.set_ard_type(ard_type)
-        slc_project_class.bursts_to_ard(
-            timeseries=False,
-            timescan=False,
-            mosaic=False,
-            overwrite=False,
-            cut_to_aoi=False,
-        )
+def test_burst_batch_ost_gtc(slc_project_class):
+    slc_project_class.set_ard_type('OST-GTC')
+    slc_project_class.bursts_to_ard(
+        timeseries=False,
+        timescan=False,
+        mosaic=False,
+        overwrite=False,
+        cut_to_aoi=False,
+    )
+
+
+def test_burst_batch_ost_rtc(slc_project_class):
+    slc_project_class.set_ard_type('OST-RTC')
+    slc_project_class.bursts_to_ard(
+        timeseries=False,
+        timescan=False,
+        mosaic=False,
+        overwrite=False,
+        cut_to_aoi=False,
+    )
 
 
 # Test GRDs to ARD kind of batch
