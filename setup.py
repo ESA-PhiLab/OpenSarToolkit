38492e477f25c3e8b7eaee911bc238bfa5ebf83d
BuddyVolly <andreasvollrath@gmx.de>
2021-09-17 15:50:34 +0200

    preparing for pypi package

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 import os
+import pathlib
 from setuptools import setup, find_packages
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 def parse_requirements(file):
     return sorted(set(
@@ -12,20 +18,22 @@ def parse_requirements(file):
 
 
 setup(
-    name='ost',
+    name='opensartoolkit',
     packages=find_packages(),
     include_package_data=True,
-    version='0.11.1',
+    version='0.12.1',
     description='High-level functionality for the inventory, download '
                 'and pre-processing of Sentinel-1 data',
     install_requires=parse_requirements('requirements.txt'),
     url='https://github.com/ESA-PhiLab/OpenSarToolkit',
-    author='Andreas Vollrath, Petr Sevcik',
-    author_email='andreas.vollrath[at]esa.int, petr.sevcik[at]eox.at',
+    author='Andreas Vollrath',
+    author_email="opensarkit@gmail.com",
     license='MIT License',
     keywords=['Sentinel-1', 'ESA', 'SAR', 'Radar',
               'Earth Observation', 'Remote Sensing',
               'Synthetic Aperture Radar'],
+    long_description=README,
+    long_description_content_type="text/markdown",
     zip_safe=False,
     setup_requires=['pytest-runner'],
     tests_require=['pytest']
