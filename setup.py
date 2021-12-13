76071f8500e349bc7435099e2da47d23b5ac568f
Kristian Bodolai <69843715+KBodolai@users.noreply.github.com>
2021-12-13 15:53:33 +0000

    Change README.md to README.rst
    pip installing from github is broken, I believe because we changed the readme format from md to rst.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
-README = (HERE / "README.md").read_text()
+README = (HERE / "README.rst").read_text()
 
 setup(
     name='opensartoolkit',
