13a49d2577197e6f7145437f45027b7a421b8380
12rambau <pierrick.rambaud49@gmail.com>
2021-12-13 18:09:33 +0100

    docs: expose source code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = "0.12.5"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autosummary"]
+extensions = ["sphinx.ext.autosummary", "sphinx.ext.viewcode"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -38,7 +38,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["**.ipynb_checkpoints"]
 
 
 # -- Options for HTML output -------------------------------------------------
