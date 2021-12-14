3b3503ca7b66c91f6d653fc4cbc5c2c1da5de048
12rambau <pierrick.rambaud49@gmail.com>
2021-12-14 09:23:17 +0100

    build: add pyproj to setup.py

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "GDAL>=2",
+        "pyproj",
         "descartes",
         "godale",
         "geopandas>=0.8",
